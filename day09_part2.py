8382bbf800e3751bfbae1b4ed452b184089ae44c
Jorge Lucangeli Obes <jlucangelio@gmail.com>
2018-12-09 15:57:45 -0500

    Day 9 part 2.

--- day09_part2.py
+++ day09_part2.py
@@ -1,52 +1,99 @@
 import re
 
+from collections import namedtuple
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.prev = None
+        self.next = None
+
+    def __str__(self):
+        return "<value=%d>" % self.value
+
+
+class CircularList(object):
+    def __init__(self):
+        self.current = None
+        self.len = 0
+
+    def insert(self, value):
+        new = Node(value)
+
+        if self.current:
+            # current <-> new <-> old_next
+            old_current = self.current
+            old_next = self.current.next
+
+            new.prev = old_current
+            new.next = old_next
+            old_current.next = new
+            old_next.prev = new
+            self.current = new
+        else:
+            self.current = new
+            self.current.prev = self.current
+            self.current.next = self.current
+
+        self.len += 1
+
+    def clockwise(self, steps):
+        for _ in range(steps):
+            self.current = self.current.next
+
+    def counterclockwise(self, steps):
+        for _ in range(abs(steps)):
+            self.current = self.current.prev
+
+    def remove(self):
+        # old_prev <-> old_next
+        ret = self.current.value
+        old_prev = self.current.prev
+        old_next = self.current.next
+        old_prev.next = old_next
+        old_next.prev = old_prev
+        self.current = old_next
+        self.len -= 1
+        return ret
+
+    def print_list(self):
+        cur = self.current
+        for _ in range(self.len):
+            print cur.value,
+            cur = cur.next
+
+        print
+
+
 INPUT = "493 players; last marble is worth 71863 points"
 nplayers, last_marble = re.match("([0-9]+) players; last marble is worth ([0-9]+) points", INPUT).groups()
 
 nplayers = int(nplayers)
-last_marble = int(last_marble)
-# last_marble = 5 * int(last_marble)
+# last_marble = int(last_marble)
+last_marble = 100 * int(last_marble)
 
 current_player = 0
 scores = dict([(p, 0) for p in range(nplayers)])
-circle = [0]
-current_marble = 0
+circle = CircularList()
+circle.insert(0)
+
 # DEBUG
-# nplayers = 10
-# last_marble = 1618
+# nplayers = 9
+# last_marble = 25
 
-circle = [12, 6, 13, 3, 14, 7, 15, 0, 16, 8, 17, 4, 18, 9, 19, 2, 20, 10, 21, 5, 22, 11, 1]
-current_marble = 20
-
-starting_marble = 23
-current_player = (starting_marble - 1 - 1) % nplayers
-next_marble = starting_marble
-
-while next_marble <= last_marble:
-    if next_marble % 23 == 1:
-        # Insert 22.
-        for _ in range(22):
-            idx = (current_marble + 2) % len(circle)
-            circle.insert(idx, next_marble)
-            current_marble = idx
-            next_marble += 1
-            current_player = (current_player + 1) % nplayers
+for next_marble in range(1, last_marble + 1):
+    # circle.print_list()
+    if next_marble % 23 != 0:
+        circle.clockwise(1)
+        circle.insert(next_marble)
+    elif next_marble % 23 == 0:
+        circle.counterclockwise(7)
+        m = circle.remove()
+        # print m
+        scores[current_player] += next_marble + m
     else:
-        if next_marble % 23 != 0:
-            idx = (current_marble + 2) % len(circle)
-            circle.insert(idx, next_marble)
-            current_marble = idx
-        elif next_marble % 23 == 0:
-            to_pop = current_marble - 7
-            m = circle.pop(to_pop)
-            scores[current_player] += next_marble + m
-            current_marble = to_pop
-        else:
-            print "error"
+        print "error"
 
-        next_marble += 1
-        current_player = (current_player + 1) % nplayers
-
-    # print next_marble, circle, current_marble
+    current_player = (current_player + 1) % nplayers
 
 print max(scores.values())
